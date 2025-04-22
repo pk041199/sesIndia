2eb644e4c2be8e2f2b43fcafb3cb318c593cc9a6
Praveen Kumar S <human.pk04@gmail.com>
2025-04-22 17:03:00 +0530

    Initial commit

---
+++ R/validate_ses_data.R
@@ -0,0 +1,11 @@
+#' Validate SES Data
+#' @param data Data frame
+#' @param income_col Column name for income
+#' @param edu_col Column name for education
+#' @param occ_col Column name for occupation
+#' @return Data frame
+#' @export
+validate_ses_data <- function(data, income_col, edu_col, occ_col) {
+  message('Validating SES data - placeholder')
+  return(data)
+}
